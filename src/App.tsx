7766e5192331604846b2c98682b254a5831486fe
Yini Shi <yini@hume.ai>
2024-03-25 20:43:26 -0400

    add posthog

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
-import { IframeFallback } from '@/components/IframeFallback';
-import { IframeGuard } from '@/components/IframeGuard';
-import { MessageListener } from '@/components/MessageListener';
-import { useConfigStore } from '@/store/config';
-import { useLayoutStore } from '@/store/layout';
-import { parentDispatch } from '@/utils/parentDispatch';
-import { Views } from '@/views/Views';
-import { TRANSCRIPT_MESSAGE_ACTION } from '@humeai/voice-embed-react';
-import { VoiceProvider } from '@humeai/voice-react';
-import { AnimatePresence } from 'framer-motion';
-import { ComponentProps } from 'react';
-import './App.css';
-import { Frame } from './components/Frame';
+import { IframeFallback } from "@/components/IframeFallback";
+import { IframeGuard } from "@/components/IframeGuard";
+import { MessageListener } from "@/components/MessageListener";
+import { useConfigStore } from "@/store/config";
+import { useLayoutStore } from "@/store/layout";
+import { parentDispatch } from "@/utils/parentDispatch";
+import { Views } from "@/views/Views";
+import { TRANSCRIPT_MESSAGE_ACTION } from "@humeai/voice-embed-react";
+import { VoiceProvider } from "@humeai/voice-react";
+import { AnimatePresence } from "framer-motion";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
+import { ComponentProps } from "react";
+
+import "./App.css";
+import { Frame } from "./components/Frame";
+
+if (typeof window !== "undefined") {
+  posthog.init(import.meta.env["VITE_PUBLIC_POSTHOG_KEY"], {
+    api_host:
+      import.meta.env["VITE_PUBLIC_POSTHOG_HOST"] || "https://app.posthog.com",
+    loaded: (posthog) => {
+      if (import.meta.env.NODE_ENV === "development") posthog.debug(); // debug mode in development
+    },
+    capture_pageview: true,
+    disable_session_recording: false,
+    enable_recording_console_log: true,
+    property_blacklist: [],
+  });
+}
 
 function App() {
   const setConfig = useConfigStore((store) => store.setConfig);
@@ -19,12 +36,12 @@ function App() {
   const open = useLayoutStore((store) => store.open);
   const setFrameSize = useLayoutStore((store) => store.setFrameSize);
 
-  const dispatchMessage: ComponentProps<typeof VoiceProvider>['onMessage'] = (
-    message,
+  const dispatchMessage: ComponentProps<typeof VoiceProvider>["onMessage"] = (
+    message
   ) => {
     if (
-      message.type === 'user_message' ||
-      message.type === 'assistant_message'
+      message.type === "user_message" ||
+      message.type === "assistant_message"
     ) {
       parentDispatch(TRANSCRIPT_MESSAGE_ACTION(message));
     }
@@ -32,29 +49,31 @@ function App() {
 
   return (
     <>
-      <IframeGuard fallback={IframeFallback}>
-        <MessageListener
-          onUpdateConfig={(config) => {
-            setConfig(config);
-          }}
-          onOpen={(dimensions) => {
-            setFrameSize(dimensions);
-            open(dimensions);
-          }}
-          onResize={(dimensions) => {
-            setFrameSize(dimensions);
-          }}
-        />
-        {config ? (
-          <Frame>
-            <AnimatePresence mode={'wait'}>
-              <VoiceProvider {...config} onMessage={dispatchMessage}>
-                <Views />
-              </VoiceProvider>
-            </AnimatePresence>
-          </Frame>
-        ) : null}
-      </IframeGuard>
+      <PostHogProvider client={posthog}>
+        <IframeGuard fallback={IframeFallback}>
+          <MessageListener
+            onUpdateConfig={(config) => {
+              setConfig(config);
+            }}
+            onOpen={(dimensions) => {
+              setFrameSize(dimensions);
+              open(dimensions);
+            }}
+            onResize={(dimensions) => {
+              setFrameSize(dimensions);
+            }}
+          />
+          {config ? (
+            <Frame>
+              <AnimatePresence mode={"wait"}>
+                <VoiceProvider {...config} onMessage={dispatchMessage}>
+                  <Views />
+                </VoiceProvider>
+              </AnimatePresence>
+            </Frame>
+          ) : null}
+        </IframeGuard>
+      </PostHogProvider>
     </>
   );
 }
