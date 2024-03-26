df302fd88fb700f5f7b3a5327488eca4b0939a70
Yini Shi <yini@hume.ai>
2024-03-25 22:05:19 -0400

    add posthog (#2)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,10 +6,11 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
     'plugin:tailwindcss/recommended',
+    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'prettier'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,26 @@ import { Views } from '@/views/Views';
 import { TRANSCRIPT_MESSAGE_ACTION } from '@humeai/voice-embed-react';
 import { VoiceProvider } from '@humeai/voice-react';
 import { AnimatePresence } from 'framer-motion';
+import posthog from 'posthog-js';
+import { PostHogProvider } from 'posthog-js/react';
 import { ComponentProps } from 'react';
+
 import './App.css';
 import { Frame } from './components/Frame';
+
+if (typeof window !== 'undefined') {
+  posthog.init(import.meta.env['VITE_PUBLIC_POSTHOG_KEY'], {
+    api_host:
+      import.meta.env['VITE_PUBLIC_POSTHOG_HOST'] || 'https://app.posthog.com',
+    loaded: (posthog) => {
+      if (import.meta.env.NODE_ENV === 'development') posthog.debug(); // debug mode in development
+    },
+    capture_pageview: true,
+    disable_session_recording: false,
+    enable_recording_console_log: true,
+    property_blacklist: [],
+  });
+}
 
 function App() {
   const setConfig = useConfigStore((store) => store.setConfig);
@@ -32,29 +49,31 @@ function App() {
 
   return (
     <>
-      <IframeGuard fallback={IframeFallback}>
-        <MessageListener
-          onUpdateConfig={(config) => {
-            setConfig(config);
-          }}
-          onOpen={(dimensions) => {
-            setFrameSize(dimensions);
-            open(dimensions);
-          }}
-          onResize={(dimensions) => {
-            setFrameSize(dimensions);
-          }}
-        />
-        {config ? (
-          <Frame>
-            <AnimatePresence mode={'wait'}>
-              <VoiceProvider {...config} onMessage={dispatchMessage}>
-                <Views />
-              </VoiceProvider>
-            </AnimatePresence>
-          </Frame>
-        ) : null}
-      </IframeGuard>
+      <PostHogProvider client={posthog}>
+        <IframeGuard fallback={IframeFallback}>
+          <MessageListener
+            onUpdateConfig={(config) => {
+              setConfig(config);
+            }}
+            onOpen={(dimensions) => {
+              setFrameSize(dimensions);
+              open(dimensions);
+            }}
+            onResize={(dimensions) => {
+              setFrameSize(dimensions);
+            }}
+          />
+          {config ? (
+            <Frame>
+              <AnimatePresence mode={'wait'}>
+                <VoiceProvider {...config} onMessage={dispatchMessage}>
+                  <Views />
+                </VoiceProvider>
+              </AnimatePresence>
+            </Frame>
+          ) : null}
+        </IframeGuard>
+      </PostHogProvider>
     </>
   );
 }
